fe64fe23035f6f606044ac3725fb0a670589c1a7
karthiga-developer <vkarthiga722@gmail.com>
2025-08-16 15:45:03 +0530

    Updated acceptdisplaystring.py

--- acceptdisplaystring.py
+++ acceptdisplaystring.py
@@ -1,3 +1,12 @@
-# Accept a string from the user and display it
+"""
+Program Name : acceptdisplaystring.py
+Author       : Karthiga
+Date         : 2025-08-16
+Purpose      : Accept a string from the user and display it.
+"""
+
+# Prompt the user to enter a string
 user_input = input("Enter a string: ")
+
+# Display the entered string
 print("You entered:", user_input)
